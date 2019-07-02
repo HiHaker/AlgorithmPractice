975df42effbba6339fd26f8c7707e58fccc597cc
Jianlong Hu <hujianlongynu@163.com>
2019-07-02 20:23:19 +0800

    队列的链表实现

---
+++ src/chapter1/Queue.java
@@ -0,0 +1,49 @@
+package chapter1;
+
+/**
+ * Created on 2019/7/2 0002
+ * BY Jianlong
+ * 先进先出队列
+ */
+public class Queue<Item> {
+    private Node first; // 指向最早添加的结点
+    private Node last; //指向最近添加的结点
+    private int N;
+
+    private class Node{
+        Item item;
+        Node next;
+    }
+
+    public boolean isEmpty(){
+        return first == null; // 或者N等于0
+    }
+
+    public int size(){
+        return N;
+    }
+
+    public void enqueue(Item item){
+        Node oldLast = last;
+        last = new Node();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()){
+            first = last;
+        }else{
+            oldLast.next = last;
+        }
+        N = N + 1;
+    }
+
+    public Item dequeue(){
+        Item item = first.item;
+        first = first.next;
+        if (isEmpty()){
+            last = null;
+        }
+        N = N - 1;
+        return item;
+    }
+
+}
